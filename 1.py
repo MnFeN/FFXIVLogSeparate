005e65a826e934c2aa8172a4828c3f81cbe1b1d6
MnFeN <85232361+MnFeN@users.noreply.github.com>
2022-11-22 07:10:06 -0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,16 @@
+__author__ = 'MnFeN'
+__version__ = '1.0'
+
 import re
 import time
+import tkinter as tk
+import tkinter.messagebox as msgbox
+from tkinter.filedialog import (askopenfilename, asksaveasfilename)
 
 filename = input('Enter the log file name in the same folder\n')
 if not re.search(r'\.[Ll][Oo][Gg] *$', filename):
     filename = filename + '.log'
-file = open(filename)
+file = open(filename, encoding='UTF-8')
 lineCount = 0
 zoneName = ''
 mapName = ''
@@ -42,8 +48,6 @@ while 1:
     re_map = re.match(r'^01\|.{34}[^|]*\|(?P<mapName>[^|]*)\|', line)
     if re_map:
         mapName = re_map.group('mapName')
-        initialize() #【检查：后启动时有没有21行】
-        startLine = lineCount + 3 #跳过01,40,02三行
         continue
     
     #zone 40|time|zoneID|regionName|zoneName|...
@@ -56,6 +60,8 @@ while 1:
     re_self = re.match(r'^02\|.{34}(?P<selfID>.{8})\|', line)
     if re_self:
         selfID = re_self.group('selfID')
+        initialize()                # 先进副本再启动 ACT 时不会有 Director 21 日志行，所以加上这行的初始化
+        startLine = lineCount + 1   # 每次进本依次会产生 01, 40, 02 三行
         continue
 
     #death 25|time|pID|...
@@ -74,27 +80,28 @@ while 1:
         totalDmgdownCount += 1
         continue
     
-    #control 33|time|.{8}|type|...
-    re_control = re.match(r'^33\|.{34}.{8}\|400000(?P<type>..)\|', line)
-    if re_control:
-        if re_control.group('type') in ['01','06']:
+    #director 33|time|.{8}|type|...
+    re_director = re.match(r'^33\|.{34}.{8}\|400000(?P<type>..)\|', line)
+    if re_director:
+        if re_director.group('type') in ['01','06']:                                    # 01:start 06:restart
             initialize()
             startLine = lineCount #要不要+1？
-        elif re_control.group('type') in ['03','10','11'] and startTimestamp != 0: #03:kill 10/11:wipe
+        elif re_director.group('type') in ['03','11','12'] and startTimestamp != 0:     # 03:kill 11/12:wipe
             duration = int(getTimestamp(line) - startTimestamp)
             durationMMSS = str(int((duration-duration%60)/60)) + ' m ' + str(duration%60) + ' s'
             #[0:startLine 1:endLine 2:startTime 3:duration 4:isWipe 5:Map 6:Zone 7-8:selfDeath/Dmgdown 9-10:totalDeath/Dmgdown]
-            fights.append([startLine, lineCount, startTimeMMSS, durationMMSS, re_control.group('type')[0]=='0', mapName, zoneName, selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount])
+            fights.append([startLine, lineCount, startTimeMMSS, durationMMSS, re_director.group('type')[0]=='0', mapName, zoneName, selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount])
         continue
 
 for i in range(len(fights)):
     print(i, fights[i][0], fights[i][1], fights[i][2], fights[i][4], fights[i][6], fights[i][5], fights[i][3], fights[i][7], '/', fights[i][9], fights[i][8], '/', fights[i][10])
-
+print(fights[24],'??')
 fightIndexes = []
 fightRawIndexes = re.split(',|，', input('Input all indexes of fights you want to extract. e.g. 0-4,7, 10 - 15 , 20\n'))
 for i in range(len(fightRawIndexes)):
     fightRawIndexes[i] = fightRawIndexes[i].replace(' ','')
-    re_fightIndex = re.match(r'(\d)+(?:[-–−—~](\d)+)?',fightRawIndexes[i])
+    re_fightIndex = re.match(r'(\d+)(?:[-–−—~](\d+))?',fightRawIndexes[i])
+    print(re_fightIndex.group(1),re_fightIndex.group(2))
     if re_fightIndex:
         if re_fightIndex.group(2) is None:
             fightIndexes.append(int(fightRawIndexes[i]))
@@ -112,7 +119,7 @@ for i in range(len(fights)):
         onLines.append(fights[i][1])
 
 file.seek(0,0)
-output_file = open(filename[:-4]+'_extract.log','w')
+output_file = open(filename[:-4]+'_extract.log','w', encoding='UTF-8')
 lineCount = 0
 switch = 1
 print(onLines)
@@ -124,11 +131,12 @@ while 1:
     lineCount += 1
     if lineCount in offLines:
         switch = 0
-    elif lineCount in onLines:
-        switch = 1
     if switch == 1:
         output_file.write(line)
-        #print(lineCount)
+    if switch == 0:
+        output_file.write('00|' + line[3:].lstrip('|')[0:35] + '0038||trash fight|0000000000000000\n')
+    if lineCount in onLines:
+        switch = 1
 file.close()
 output_file.close()
 input('Logs extracted. Press enter to exit.\n')
