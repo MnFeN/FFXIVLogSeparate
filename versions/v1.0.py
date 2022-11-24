ccc48914e7d533e1b27a950c3933853df792ae03
MnFeN <85232361+MnFeN@users.noreply.github.com>
2022-11-23 16:49:05 -0800

    Update v1.0.py

--- versions/v1.0.py
+++ versions/v1.0.py
@@ -1,5 +1,5 @@
 __author__ = 'MnFeN'
-__version__ = '0.1'
+__version__ = '1.0'
 
 import re
 import time
@@ -9,23 +9,33 @@ import tkinter.messagebox as msgbox
 from tkinter.filedialog import (askopenfilename, asksaveasfilename)
 import ctypes
 
-def initialize():
-    global selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount, startTimeMMSS, startTimestamp, startLine
-    selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount, startTimeMMSS, startTimestamp, startLine = 0,0,0,0,'00:00',0,0
-
 def getTimestamp(line):
-    timeArray = time.strptime(line[3:22], "%Y-%m-%dT%H:%M:%S")
+    timeArray = time.strptime(line[3:].lstrip('|')[:19], "%Y-%m-%dT%H:%M:%S") # 开头日志行类型为2-3位
     timestamp = time.mktime(timeArray)
     return timestamp
 
+def selectAll():
+    isSelect = checkVars[0].get()
+    for index in range(1,len(checkVars)):
+        checkVars[index].set(isSelect)
+
 def readLogFile():
-    global selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount, startTimeMMSS, startTimestamp, startLine
+    global filename, file, fights
     filename = askopenfilename(title="Select the log file", filetypes=[("日志文件", ".log")])
     file = open(filename, encoding='UTF-8')
-    lineCount = 0
-    mapName = ''
-    selfID = ''
     fights = []
+    
+    lineCount = 0
+    selfID = ''
+    mapName = ''
+
+    def initialize():
+        nonlocal selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount
+        selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount = 0,0,0,0
+
+        nonlocal startTimeStr, startTimestamp, startLine
+        startTimeStr, startTimestamp, startLine = '00:00:00',0,0
+    
     initialize()
     
     while 1:
@@ -41,13 +51,14 @@ def readLogFile():
         #重置后第一次 1.{7} -> 4.{7} 的 Action：记录开始时间
         if startTimestamp == 0 and re.match(r'^2[12]\|.{34}1.{7}\|[^|]*\|[^|]*\|[^|]*\|4', line):
             startTimestamp = getTimestamp(line)
-            startTimeMMSS = line[14:19]
+            startTimeStr = line[14:22]
             continue
 
         #map 01|time|mapID|mapName|...
         re_map = re.match(r'^01\|.{34}[^|]*\|(?P<mapName>[^|]*)\|', line)
         if re_map:
             mapName = re_map.group('mapName')
+            startLine01 = lineCount
             continue
 
         #self 02|time|selfID|...
@@ -82,18 +93,24 @@ def readLogFile():
                 startLine = lineCount #要不要+1？
             elif re_director.group('type') in ['03','11','12'] and startTimestamp != 0:     # 03:kill 11/12:wipe
                 duration = int(getTimestamp(line) - startTimestamp)
-                durationMMSS = str(int((duration-duration%60)/60)) + "'" + str(duration%60) + '"'
-                #[0:startLine 1:endLine 2:startTime 3:duration 4:isWipe 5:Map 6-7:selfDeath/Dmgdown 8-9:totalDeath/Dmgdown]
-                fights.append([startLine, lineCount, startTimeMMSS, durationMMSS, re_director.group('type')[0]=='0', mapName, selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount])
+                durationStr = '{:0>2d}\'{:0>2d}"'.format(int((duration-duration%60)/60), int(duration%60))  # time: 01'35"
+                #[0:startLine 1:endLine 2:startTime 3:duration 4:isWipe 5:Map 6-7:selfDeath/Dmgdown 8-9:totalDeath/Dmgdown 10:startLine01]
+                fights.append([
+                    startLine, lineCount, startTimeStr, durationStr, re_director.group('type')[0]=='0', 
+                    mapName, selfDeathCount, selfDmgdownCount, totalDeathCount, totalDmgdownCount, startLine01])
             continue
     
-    #初始化表格Frame
+    #清空下方的表格Frame
     for widget in tableFrame.winfo_children():
         widget.destroy()
+
+    global table, checkVars
     table=[[]]
+    checkVars = []
 
     #标题行
-    table[0].append(ttk.Label(tableFrame, text=' '))
+    checkVars.append(tk.IntVar())
+    table[0].append(ttk.Checkbutton(tableFrame, text='', variable=checkVars[0],command=selectAll))
     table[0].append(ttk.Label(tableFrame, text='No.'))
     table[0].append(ttk.Label(tableFrame, text='Start'))
     table[0].append(ttk.Label(tableFrame, text='Duration'))
@@ -101,19 +118,21 @@ def readLogFile():
     table[0].append(ttk.Label(tableFrame, text='D'))
     table[0].append(ttk.Label(tableFrame, text='D -'))
     for i in range(len(table[0])):
-        table[0][i].grid(row=0,column=i)
+        table[0][i].grid(row=0,column=i,padx=10,pady=5)
     #下面每行
     for i in range(len(fights)):
         table.append([])
-        table[-1].append(ttk.Label(tableFrame,text='✓'))
-        table[-1].append(ttk.Label(tableFrame,text=str(i+1)))
-        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][2])))
-        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][3])))
-        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][5])))
-        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][6])+'/'+str(fights[i][8])))
-        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][7])+'/'+str(fights[i][9])))
+        isWipeColor = ['#d16969','#4ec9b0'][fights[i][4]] # wipe=red, kill=green
+        checkVars.append(tk.IntVar())
+        table[-1].append(ttk.Checkbutton(tableFrame, text='', variable=checkVars[-1]))         #table[row][0]: checkbox
+        table[-1].append(ttk.Label(tableFrame,text=str(i+1),foreground=isWipeColor))           #table[row][1]: index
+        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][2]),foreground=isWipeColor))  #table[row][2]: startTime
+        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][3]),foreground=isWipeColor))  #table[row][3]: duration
+        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][5])))                         #table[row][4]: map
+        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][6])+'/'+str(fights[i][8])))   #table[row][5]: d
+        table[-1].append(ttk.Label(tableFrame,text=str(fights[i][7])+'/'+str(fights[i][9])))   #table[row][6]: d-
         for col in range(len(table[-1])):
-            table[-1][col].grid(row=i+1,column=col)
+            table[-1][col].grid(row=i+1,column=col,padx=10,pady=5)
 
 def saveLogFile():
     fightIndexes = []
@@ -153,7 +172,7 @@ def saveLogFile():
         if switch == 1:
             output_file.write(line)
         if switch == 0:
-            output_file.write('00|' + line[3:].lstrip('|')[0:35] + '0038||trash fight|0000000000000000\n')
+            output_file.write('00|' + line[3:].lstrip('|')[:35] + '0038||trash fight|0000000000000000\n')
         if lineCount in onLines:
             switch = 1
     file.close()
@@ -169,10 +188,10 @@ screen_x = window.winfo_screenwidth()
 screen_y = window.winfo_screenheight()
 window.geometry('%dx%d+%d+%d' % (int(screen_x*0.7), int(screen_y*0.7), int(screen_x*0.15), int(screen_y*0.12)))
 
-button_import = ttk.Button(window, text='Import Log',command=readLogFile)
-button_export = ttk.Button(window, text='Export Log')
-button_import.place(relx=0.35,relwidth=0.15,rely=0.05,relheight=0.06,anchor='center')
-button_export.place(relx=0.65,relwidth=0.15,rely=0.05,relheight=0.06,anchor='center')
+button_import = ttk.Button(window, text='Import Log', command=readLogFile)
+button_export = ttk.Button(window, text='Export Log', command=saveLogFile)
+button_import.place(relx=0.5-0.15,relwidth=0.15,rely=0.05,relheight=0.06,anchor='center')
+button_export.place(relx=0.5+0.15,relwidth=0.15,rely=0.05,relheight=0.06,anchor='center')
 
 tableFrame = ttk.Frame(window, borderwidth=1)
 tableFrame.place(relx=0.5,rely=0.55,anchor='center')
